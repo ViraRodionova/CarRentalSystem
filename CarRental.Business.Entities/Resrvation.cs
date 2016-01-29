30a146d95a812ec7d5be35b28d013f7a8e38d3b2
ViraRodionova <bepa.rdnv@gmail.com>
2016-01-29 22:28:43 +0200

    work is started

---
+++ CarRental.Business.Entities/Resrvation.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+using Core.Common.Core;
+using Core.Common.Contracts;
+
+namespace CarRental.Business.Entities
+{
+    [DataContract]
+    public class Resrvation : EntityBase, IIdentifiableEntity, IAccountOwnedEntity
+    {
+        [DataMember]
+        public int ReservationId { get; set; }
+        [DataMember]
+        public int AccountId { get; set; }
+        [DataMember]
+        public int CarId { get; set; }
+        [DataMember]
+        public DateTime RentalDate { get; set; }
+        [DataMember]
+        public DateTime ReturnDate { get; set; }
+
+        #region IIdentifiableEntity Members
+        
+        public int EntityId
+        {
+            get { return ReservationId; }
+            set { ReservationId = value; }
+        }
+
+        #endregion
+                
+        #region IAccountOwnedEntity Members
+        
+        public int OwnerAccountId
+        {
+            get { return AccountId; }
+        }
+
+        #endregion
+    }
+}
---
+++ Core.Common/Core/ObjectBase.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+
+namespace Core.Common.Core
+{
+    public abstract class ObjectBase : INotifyPropertyChanged 
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChaned(string propertyName)
+        {
+            OnProertyChanged(propertyName, true);
+        }
+
+        protected virtual void OnProertyChanged(string propertyName, bool makeDirty)
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            
+            if (makeDirty)
+                _IsDirty = true;
+        }
+
+        bool _IsDirty;
+        public bool IsDirty
+        {
+            get { return _IsDirty; }
+        }
+
+        //этот комментария находится здесь для проверки работоспособнрости Git
+    }
+}
