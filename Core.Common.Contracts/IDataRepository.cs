79416f9de2b1f2e1cfbd0dcb340b517c1d419f9d
ViraRodionova <bepa.rdnv@gmail.com>
2016-02-06 20:35:16 +0200

    + IDataRepository interface

---
+++ Core.Common.Contracts/IDataRepository.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Core.Common.Contracts
+{
+    /*
+     *  Если есть инт-с с Generics, то он создает всегда инт-с без Generics
+     */
+    public interface IDataRepository
+    {
+
+    }
+
+    public interface IDataRepository<T> : IDataRepository
+        where T : class, IIdentifiableEntity, new()
+    {
+        T Add(T entity);
+
+        void Remove(T entity);
+
+        void Remove(int id);
+
+        T Update(T entity);
+
+        IEnumerable<T> Get();
+
+        T Get(int id);
+    }
+}
